19c10ea9c9ff5d240edf167839185ca01220acc8
ShoaibShoukat2 <107969167+ShoaibShoukat2@users.noreply.github.com>
2023-09-13 21:38:21 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-py88=o#-$$bgtaxv^w$rj))v5_h0pi-5m5!j!#u_%mgdsgfk(g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
